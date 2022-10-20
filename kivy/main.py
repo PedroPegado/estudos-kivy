db6687232cb833346e7d2b63fa116b807255a2d0
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-10-20 10:47:52 -0300

    primeiras telas

---
+++ kivy/main.py
@@ -0,0 +1,38 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, ScreenManager
+from functions import callbacklogin, callbackregister
+
+Window.size = (350, 580)
+
+global screen
+screen = ScreenManager()
+
+class SplashScreen(Screen):
+    pass
+
+
+class InitialScreen(Screen):
+    pass
+
+
+class LoginScreen(Screen):
+    pass
+
+
+screen.add_widget(SplashScreen(name='splash'))
+screen.add_widget(InitialScreen(name='inicio'))
+screen.add_widget(LoginScreen(name='login'))
+
+
+
+class Apps(MDApp):
+    def build(self):
+        kv = Builder.load_file("tela.kv")
+        screen = kv
+        return screen
+    
+if __name__ == '__main__':
+    Apps().run()
